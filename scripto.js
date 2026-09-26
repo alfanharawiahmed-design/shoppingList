46c701274864f097d02fdf10fe58fb24dbb38a5d
alfanharawiahmed-design <alfanharawiahmed@gmail.com>
2026-09-26 08:21:38 +0300

    Hey8

--- scripto.js
+++ scripto.js
@@ -3,6 +3,8 @@ const inputButton = document.querySelector('.btn');
 const ul = document.querySelector('#item-list');
 const xmarkList = document.querySelectorAll('li button');
 const clearBtn = document.getElementById('clear');
+const filter = document.getElementById('filter');
+
 
 
 function addItem(e)
@@ -14,28 +16,38 @@ function addItem(e)
         li.textContent = inputText.value
         const button = document.createElement('button');
         button.className = 'remove-item btn-link text-red';
-        button.addEventListener('click', removeItem);
         const i = document.createElement('i');
         i.className = 'fa-solid fa-xmark';
+        i.addEventListener('click',removeItem)
     
         button.appendChild(i);
         li.appendChild(button);
         ul.appendChild(li);
         inputText.value = '';
+        clearBtn.style.display = '';
+        filter.style.display = '';
+        
     }
     
 }
 
 function removeItem(e)
 {
-    e.target.parentElement.parentElement.remove()
+    if (e.target.parentElement.parentElement.tagName === 'LI') { e.target.parentElement.parentElement.remove(); hide(); }
 };
 
-function clearItems(e)
+function clearItems()
 {
-    if (ul.innerHTML == '') { alert('السلة فارغة لاتحتوي حاجات') } else { ul.innerHTML = '';}
+    if (ul.children.length === 0) { alert('السلة فارغة لاتحتوي عناصــر'); } else { ul.innerHTML = ''; };
+    hide();
 }
-xmarkList.forEach(item => { item.addEventListener('click', removeItem) });
 
+function hide()
+{
+    if (ul.children.length === 0) { filter.style.display = 'none'; clearBtn.style.display='none'}
+};
+
+hide();
+ul.addEventListener('click', removeItem);
 inputButton.addEventListener('click', addItem);
 clearBtn.addEventListener('click',clearItems)
